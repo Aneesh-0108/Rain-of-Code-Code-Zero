6fc6569f39efe4604254486900ca2a62cce609b3
Aneesh-0108 <cpaneesh2006@gmail.com>
2025-09-12 23:30:26 +0530

    Made changes to backend folder

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,6 +5,18 @@ from src.decorators import require_auth, require_role
 from src.services import events_service, registration_service
 from flask import Response
 from src.utils.ics import build_calendar
+from flask_cors import CORS
+
+
+
+app = Flask(__name__)
+
+# Allow only your local dev frontends
+CORS(app, origins=[
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:8000"
+])
 
 
 
